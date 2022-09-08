a95af4954dce339d7cbe976d4982c3388ca82def
RichieOrito <oritorichie2@gmail.com>
2022-09-08 17:43:03 +0300

    added props to make the components dynamic

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio(props)}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {Links(props)}
     </div>
   );
+}
+
+function Links(props){
+  return(
+      <div>
+      <h3>Links</h3>
+          <a href={props.github}>{props.github}</a>
+          <a href={props.linkedIn}>{props.linkedIn}</a>
+      </div>
+  )
+}
+
+function bio(props){
+  if(!props.bio){
+    return null
+  }else{
+    return <p>{props.bio}</p>
+  }
 }
 
 export default About;
